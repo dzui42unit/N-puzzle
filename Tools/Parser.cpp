ce09b67f763daf881045c1c863006da676e520c5
virus-on <virus-on@mail.ru>
2018-10-14 19:55:40 +0300

    Added some comments

--- Tools/Parser.cpp
+++ Tools/Parser.cpp
@@ -18,14 +18,18 @@ Parser::Parser(const char* name)
 
     // Split by \n to get all rows, use it's count a edge size of the gamefield
     auto splitted = Common::tools::split(content, '\n');
+    // remove comments
     RemoveComments(splitted);
+    // get size and remove size field
     GetSize(splitted);
     if (hasError_)
     {
         std::cout << "Error: Can't get size!" << std::endl;
         return;
     }
+
     std::vector<std::string> strData; // vector to store data in string format
+    // get all data in str format
     for (auto& str : splitted)
     {
         Common::tools::trim(str);
@@ -46,7 +50,7 @@ Parser::Parser(const char* name)
         return;
     }
 
-    // If data is valid convert it to int
+    // If str data is valid convert it to int
     if ( ValidateData(strData) )
     {
         data_ = new int[size_ * size_];
@@ -81,14 +85,17 @@ bool Parser::HasErrors()
 
 void Parser::RemoveComments(std::vector<std::string>& data)
 {
+    // traverse vector and delete all char parts starting from # 
     for (int i = 0; i < data.size(); i++)
         data[i] = data[i].substr(0, data[i].find('#'));
+    // remove empty strings from vector
     auto new_end = std::remove(data.begin(), data.end(), "");
     data.erase(new_end, data.end());
 }
 
 void Parser::GetSize(std::vector<std::string>& data)
 {
+    // check that 'size field' exists and is valid
     for (char c : data[0])
     {
         if (!isdigit(c))
@@ -97,7 +104,9 @@ void Parser::GetSize(std::vector<std::string>& data)
             return ;
         }  
     }
+    // get size in int format
     size_ = std::stoi(data[0]);
+    // delete now useless size field from vector
     data.erase(data.begin());
 }
 
